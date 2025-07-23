2ac2261a0821fe4668581f2aa0a8a6f0d8718b84
Uribarber <urielenriquepl@gmail.com>
2025-07-23 14:19:23 -0600

    Abstraccion.java
    Cree el programa de ejemplo de Abstraccion con un ejemplo de un teléfono.

---
+++ PROGRAMAS/poo/Basics/src/abstraccion/Abstraccion.java
@@ -0,0 +1,22 @@
+package abstraccion;
+//ABSTRACCION
+public class Abstraccion {
+    public static void main(String[] args) {
+        Telefono obj = new Telefono("Blanco", "Android");
+        obj.hacerLlamadaTelefonica();
+    }
+}
+
+class Telefono {
+    private String color;
+    private String marca;
+
+    public Telefono(String color, String marca) {
+        this.color = color;
+        this.marca = marca;
+    }
+
+    public void hacerLlamadaTelefonica() {
+        System.out.println("Haciendo una llamada...");
+    }
+}
